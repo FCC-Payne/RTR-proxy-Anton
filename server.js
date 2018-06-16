c6535ac02d83656a30a4fcb5aab88ce6fad5324c
ACRUNNER <acrunner1992@gmail.com>
2018-06-15 21:56:05 -0700

    finished!

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const app = express();
 app.get('/forms', (req, res) => {
-	res.redirect(`http://localhost:3003/100/bundle.js`);
+	console.log('3003');
+	res.redirect(`http://localhost:3003/${req.params.id}/bundle.js`);
 });
-app.get('/forms/:id', (req, res) => {
-	console.log(req.params.id);
-	res.redirect(`http://localhost:3003/forms/${req.params.id}`)
+app.get('/:id/photo-viewer', (req, res) => {
+	res.redirect(`http://localhost:3001/${req.params.id}/photo-viewer/bundle.js`);
+});
+app.get('/:id/photo-viewer/styles', (req, res) => {
+  res.redirect(`http://localhost:3001/${req.params.id}/photo-viewer/styles.css`);
+});
+app.get('/:id/carousel/bundle', (req, res) => {
+  console.log('3004 bundle');
+  res.redirect(`http://localhost:3004/${req.params.id}/bundle.js`);
+});
+app.get('/:id/carousel/styles', (req, res) => {
+  console.log('3004 style');
+  res.redirect(`http://localhost:3004/${req.params.id}/styles.css`);
+});
+app.get('/:id/reviews/bundle', (req, res) => {
+  res.redirect(`http://localhost:3002/${req.params.id}/bundle.js`);
+});
+app.get('/:id/reviews/styles', (req,res) => {
+  res.redirect(`http://localhost:3002/${req.params.id}/stylesheet.css`);
 });
 app.use('/:id', express.static('public'));
 app.listen(3000);
